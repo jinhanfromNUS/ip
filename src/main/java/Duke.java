56ce3a7bd87054570d8c87879691c99c558f13c3
jinhanfromNUS <111408810+jinhanfromNUS@users.noreply.github.com>
2024-01-25 03:28:28 +0800

    Add files via upload

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void messageWithHorizontalLines(String message) {
+        System.out.println("____________________________________________________________\n" +
+                           message + "\n" +
+                           "____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        String botName = "Hammy";
+        String welcomeStr = " Hello! I'm " + botName + "\n What can I do for you?";
+        String byeStr = "Bye. Hope to see you again soon!";
+
+        System.out.println(welcomeStr);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                // If the user enters "bye", exit the loop and say goodbye
+                messageWithHorizontalLines(" Bye. Hope to see you again soon!");
+                break;
+            } else {
+                // For any other input, just display it
+                messageWithHorizontalLines(" " + userInput);
+            }
+        }
+
+        scanner.close();
     }
 }
